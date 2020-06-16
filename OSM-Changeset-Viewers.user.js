63c75c0d3de0132d11fa9cf2409f07161570364f
Marco Antonio <marcoantoniofrias@gmail.com>
2020-06-16 00:26:08 -0400

    initial commit

---
+++ OSM-Changeset-Viewers.user.js
@@ -0,0 +1,110 @@
+// ==UserScript==
+// @name         OSM Changeset Viewers
+// @namespace    https://github.com/51114u9/osm-changeset-viewers
+// @version      0.1
+// @description  Adds changeset viewers in OSM.org
+// @author       51114u9
+// @homepage     https://github.com/51114u9/osm-changeset-viewers
+// @updateURL    https://github.com/51114u9/osm-changeset-viewers/raw/master/OSM-Changeset-Viewers.user.js
+// @downloadURL  https://github.com/51114u9/osm-changeset-viewers/raw/master/OSM-Changeset-Viewers.user.js
+// @supportURL   https://github.com/51114u9/osm-changeset-viewers/issues
+// @match        https://www.openstreetmap.org/node/*
+// @match        https://www.openstreetmap.org/way/*
+// @match        https://www.openstreetmap.org/relation/*
+// @match        https://www.openstreetmap.org/history
+// @match        https://www.openstreetmap.org/user/*/history
+// @match        https://www.openstreetmap.org/history/friends
+// @grant        none
+// ==/UserScript==
+
+var changeset_viewers = {
+    achavi: "https://nrenner.github.io/achavi/?changeset=<number>",
+    achaviText: "[A]",
+    osmcha: "https://osmcha.org/changesets/<number>/",
+    osmchaText: "[O]"
+};
+
+function createLink(url, urlText, changesetNumber) {
+    let link = document.createElement("a");
+    link.style = "font-size:smaller;";
+    link.href = url.replace("<number>", changesetNumber);
+    link.innerHTML = urlText;
+    link.target = "_blank";
+    return link;
+}
+
+function createChangesetViewers(element, changesetNumber) {
+    let achavi = createLink(changeset_viewers.achavi, changeset_viewers.achaviText, changesetNumber);
+    let osmcha = createLink(changeset_viewers.osmcha, changeset_viewers.osmchaText, changesetNumber);
+
+    element.appendChild(achavi);
+    element.appendChild(document.createTextNode(" "));
+    element.appendChild(osmcha);
+
+    return element;
+}
+
+function updateOSMElements() {
+    let sidebarContent = document.getElementById("sidebar_content");
+
+    let changesetDetails = sidebarContent.children[1].children[2];
+    let changesetNumber = changesetDetails.lastElementChild.innerText;
+
+    createChangesetViewers(changesetDetails, changesetNumber);
+}
+
+function updateHistory() {
+    let sidebarContent = document.getElementById("sidebar_content");
+
+    let changesetList = sidebarContent.children[1].children[0].children;
+
+    for (let changeset of changesetList) {
+        let changesetDetails = changeset.children[2];
+        let changesetNumber = JSON.parse(changeset.dataset.changeset).id;
+
+        createChangesetViewers(changesetDetails, changesetNumber);
+    }
+}
+
+function updateUserChangesets() {
+    let sidebarContent = document.getElementById("sidebar_content");
+
+    let changesetList = sidebarContent.children[1].children[0].children;
+
+    for (let changeset of changesetList) {
+        let changesetDetails = changeset.children[2];
+        let changesetNumber = JSON.parse(changeset.dataset.changeset).id;
+
+        createChangesetViewers(changesetDetails, changesetNumber);
+    }
+}
+
+function updateHistoryFriends() {
+    let sidebarContent = document.getElementById("sidebar_content");
+
+    let changesetList = sidebarContent.children[1].children[0].children;
+
+    for (let changeset of changesetList) {
+        let changesetDetails = changeset.children[2];
+        let changesetNumber = JSON.parse(changeset.dataset.changeset).id;
+
+        createChangesetViewers(changesetDetails, changesetNumber);
+    }
+}
+
+(function() {
+    'use strict';
+
+    if (window.location.href.match(/\/(node|way|relation)\//g)) {
+        updateOSMElements();
+    } else if (window.location.href.match(/\/user\/.*\/history/g)) {
+        window.setTimeout(updateUserChangesets, 2000);
+        updateUserChangesets();
+    } else if (window.location.href.match(/\/history\/friends/g)) {
+        window.setTimeout(updateHistoryFriends, 2000);
+        updateHistoryFriends();
+    } else if (window.location.href.match(/\/history/g)) {
+        window.setTimeout(updateHistory, 2000);
+        updateHistory();
+    }
+})();
---
+++ README.md
@@ -0,0 +1,35 @@
+
+# OSM Changeser Viewers
+
+Adds links to Changeset Viewers ([Achavi](https://wiki.openstreetmap.org/wiki/Achavi) and
+[OSMCha](https://wiki.openstreetmap.org/wiki/OSMCha)) at [osm.org](htttps://openstreetmap.org)
+
+## Screenshots
+
+Before:
+
+![before](https://raw.githubusercontent.com/51114u9/osm-changeset-viewers/master/osm-changeset-viewers_before.jpg)
+
+After:
+
+![after](https://raw.githubusercontent.com/51114u9/osm-changeset-viewers/master/osm-changeset-viewers_after.jpg)
+
+## Requirements
+
+You need install the add-on or extension for your modern browser:
+
+* Firefox [TamperMonkey](https://addons.mozilla.org/firefox/addon/tampermonkey/)
+* Chrome/Chromium [TamperMonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo)
+
+## Install
+
+Click [here](https://raw.githubusercontent.com/51114u9/osm-changeset-viewers/master/OSM-Changeset-Viewers.user.js),
+click on "Install" and wait for install
+
+## Apply Script
+
+Just reload [osm.org](https://openstreetmap.org/) and enjoy!
+
+## ToDo
+
+* Investigate how to intercept AJAX requests
